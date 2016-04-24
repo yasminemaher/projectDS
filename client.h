e671bef0df408c6018692a2d7d1457d3f951617b
yasminemaher <yasminemaher@live.com>
2016-04-24 13:44:01 +0200

    Update client.h

--- client.h
+++ client.h
@@ -2,12 +2,29 @@
 #include <string.h>
 #include <iostream>
 using namespace std;
-class account
+
+class client
 {
-private: 
+private:
 	double balance;
+	string name;
+	bool vip;
 public:
-	account() { balance = 0;  }
+	
+	client() {b=0; }
+	client(string s, bool importance) { name = s; b=0 ; vip = importance; }
+	//void closeAccount(){ ~a; }
+	void transfer(client c, double amount)
+	{
+		if (amount<balance)
+			cout << "Can't complete operation: balance doesn't cover amount";
+		else
+		{
+			balance -= amount;
+			c.balance += amount;
+		}
+	}
+	
 	
 	void setBalance(double b) { balance = b; }
 	double getBalance() { return balance; }
@@ -19,28 +36,4 @@ public:
 			balance -= w;
 	}
 	void depose(double d) { balance += d; }
-
-	friend class client;
-};
-class client
-{
-private:
-	account a;
-	string name;
-	bool vip;
-public:
-	
-	client() {a.setBalance(0); }
-	client(string s, bool importance) { name = s; a.setBalance(0); vip = importance; }
-	//void closeAccount(){ ~a; }
-	void transfer(client c, double amount)
-	{
-		if (amount<a.balance)
-			cout << "Can't complete operation: balance doesn't cover amount";
-		else
-		{
-			a.balance -= amount;
-			c.a.balance += amount;
-		}
-	}
 };
